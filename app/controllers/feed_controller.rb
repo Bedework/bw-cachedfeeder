b5468594828772214d421bacfbb7c88ba6927ac2
Barry Leibson <bleibson@gmail.com>
2009-11-05 17:39:29 +0000

    Many updates to reflect new project design.

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,34 +3,33 @@ require 'httpclient'
 # routes.rb determines url parameters and url segment configuration
 class FeedController < ApplicationController 
   # Rails caching for each action
-  caches_page :calendar
-  caches_page :eventList
-  caches_page :ics
+  #caches_page :calendar
+  caches_page :genFeedRange
+  caches_page :genFeedDays
+  caches_page :icsRange
+  caches_page :icsDays
   caches_page :event
   caches_page :external
   
   #before_filter :inspect
   def addExtension
-    if params[:xmlRss]
-      ActionController::Base.page_cache_extension = '.' + params[:xmlRss]
-      headers["Content-Type"] = APP_CONFIG['content_type'][params[:xmlRss]]
-      
+    if params[:skin]
+      ActionController::Base.page_cache_extension = '.' + params[:skin]
+      headers["Content-Type"] = APP_CONFIG['content_type'][params[:skin]]  
       return
     end
     
-    if params[:action] == 'external'
-      ActionController::Base.page_cache_extension = '.rss'
-      headers["Content-Type"] = APP_CONFIG['content_type']['rss']
-      
-      return
-    end
-    
-    if params[:action] == 'ics'
+    if params[:action] == 'icsDays' || params[:action] == 'icsRange'
       ActionController::Base.page_cache_extension = '.ics'
       headers["Content-Type"] = APP_CONFIG['content_type']['ics']
-      
       return
     end
+    
+     #if params[:action] == 'external'
+      #  ActionController::Base.page_cache_extension = '.rss'
+      #  headers["Content-Type"] = APP_CONFIG['content_type']['rss']
+      #  return
+      #end
   end
   
   def getFeed(feedUrl) # uses httpclient gem
@@ -41,43 +40,59 @@ class FeedController < ApplicationController
    
     return clnt.get_content(target)
   end
-
-  def calendar # Bedework calendar web client (uses setViewPeriod.do)
-	 currUrl = FeedModel.new('calendar', params)
-	 target = currUrl.buildUrl
-   logger.info("\nFEED: calendar URL is #{target}\n")
-	 @xmlOutput = getFeed(target)
-   addExtension()
-  end
   
-  def eventList # Bedework Events List web client (uses setViewPeriod.do)
-	  currUrl = FeedModel.new('list', params)
+  def genFeedDays
+	  currUrl = FeedModel.new('genFeedDays', params)
 	  target = currUrl.buildUrl
-    # brutal but effective ActionController::Base.page_cache_extension        = '.rss'
 	  logger.info("\nFEED: list URL is #{target}\n")
 	  @xmlOutput = getFeed(target)
     addExtension()
 	end
 	
-  def ics # Bedework ICS interface, accepts ~ seperated list of categories
-    currUrl = FeedModel.new('ics', params)
+  def icsDays
+    currUrl = FeedModel.new('icsDays', params)
     target = currUrl.buildUrl
+    logger.info("\nFEED: list URL is #{target}\n")
     @xmlOutput = getFeed(target)
     addExtension()
   end
   
-  def event # request specific event 
-    currUrl = FeedModel.new('event', params)
+  def genFeedRange
+	  currUrl = FeedModel.new('genFeedRange', params)
+	  target = currUrl.buildUrl
+	  logger.info("\nFEED: list URL is #{target}\n")
+	  @xmlOutput = getFeed(target)
+    addExtension()
+	end
+	
+  def icsRange
+    currUrl = FeedModel.new('icsRange', params)
     target = currUrl.buildUrl
+    logger.info("\nFEED: list URL is #{target}\n")
     @xmlOutput = getFeed(target)
     addExtension()
   end
   
-  def external
-    currUrl = FeedModel.new('external', params)
-    target = currUrl.buildUrl
-    @xmlOutput = getFeed(target)
-    addExtension()
-  end
+  # def calendar # Bedework calendar web client (uses setViewPeriod.do)
+  #	 currUrl = FeedModel.new('calendar', params)
+  #	 target = currUrl.buildUrl
+  #  logger.info("\nFEED: calendar URL is #{target}\n")
+  #	 @xmlOutput = getFeed(target)
+  #  addExtension()
+  #  end
+  
+  #def event # request specific event 
+  #  currUrl = FeedModel.new('event', params)
+  #  target = currUrl.buildUrl
+  #  @xmlOutput = getFeed(target)
+  #  addExtension()
+  #end
+  
+  #def external
+  #  currUrl = FeedModel.new('external', params)
+  #  target = currUrl.buildUrl
+  #  @xmlOutput = getFeed(target)
+  #  addExtension()
+  #end
   
 end
