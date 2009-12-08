63bc7229ccd31bdffaa6988f2b62249091d80550
Barry Leibson <bleibson@gmail.com>
2009-12-08 02:55:57 +0000

    o  Single events now routed to caching system o  Bullet-proofed a wee bit.

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,39 @@ ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Used for feeds (other than ICS) where the number of days is specified 
-  map.connect 'genFeedDays/:days/:skin/:group/:categories', :controller => 'feed', :action => 'genFeedDays',
-  :requirements => {:categories => /.*/}
- 
-  # Used for ICS feeds where the number of days is specified.
-  map.connect 'icsDays/:days/:group/:categories', :controller=> 'feed', :action => 'icsDays',
-  :requirements => {:categories => /.*/}
+  map.connect 'genFeedDays/:days/:skin/:group/:categories', :controller => 'feed', :action => 'genFeedDays'
   
   # Used for feeds (other than ICS) where the start and end dates are specified.
   map.connect 'genFeedRange/:startDate/:endDate/:skin/:group/:categories', :controller => 'feed', :action => 'genFeedRange',
-  :requirements => {:categories => /.*/, :startDate => /\d{4}-\d{2}-\d{2}/, :endDate => /\d{4}-\d{2}-\d{2}/}
+  :requirements => {:startDate => /\d{8}/, :endDate => /\d{8}/}
 
+  # Used for feeds (other than ICS) where the period (week, month, year) are specified
+  map.connect 'genFeedPeriod/:period/:date/:skin/:group/:categories', :controller => 'feed', :action => 'genFeedPeriod',
+  :requirements => {:date => /\d{8}/}
+   
+  # Used for ICS feeds where the number of days is specified.
+  map.connect 'icsDays/:days/:group/:categories', :controller=> 'feed', :action => 'icsDays'
+  
   # Used for ICS feeds where the start and end dates are specfied
   map.connect 'icsRange/:startDate/:endDate/:group/:categories', :controller=> 'feed', :action => 'icsRange',
-  :requirements => {:categories => /.*/, :startDate => /\d{4}-\d{2}-\d{2}/, :endDate => /\d{4}-\d{2}-\d{2}/}
+ :requirements => {:startDate => /\d{8}/, :endDate => /\d{8}/}
   
+  # Used for feeds (other than ICS) where the period (week, month, year) are specified
+  map.connect 'icsFeedPeriod/:period/:date/:skin/:group/:categories', :controller => 'feed', :action => 'icsPeriod',
+  :requirements => {:date => /\d{8}/}
+  
+  # Used by Feed/Widget Builder to populate category and group lists.
   map.connect 'categories/:skin/:objName', :controller=> 'feed', :action => 'categories'
   map.connect 'groups/:skin/:objName', :controller=> 'feed', :action => 'groups'
+  
+  # Used for specific event
+  map.connect 'event/:skin/:recurrenceId/:guid', :controller => 'feed', :action => 'event',
+  :requirements => {:guid => /.*/}  #needed when you have '.' in one of your path elements
+
+    # requires guid extension "@mysite.edu" to have .edu (or other) mime type mapped to html in Apache. otherwise cache is served as plain text
+  
+  
+  
   
 
   # Allow downloading Web Service WSDL as a file with an extension
@@ -33,11 +49,7 @@ ActionController::Routing::Routes.draw do |map|
   # map.connect 'timeblock/:viewType/:xmlRss/:group/:categories', :controller => 'feed', :action => 'timeBlock',
   # :requirements => {:categories => /.*/, :group => /\({1}.*\){1}/}
 
-# Request specific event
-#  map.connect 'event/:suite/:skin/:subId/:calPath/:recurrenceId/:guid', :controller => 'feed', :action => 'event',
-#  :requirements => {:guid => /.*/} # assumes root 'public' calPath folder
-  # requires guid extension "@mysite.edu" to have .edu (or other) mime type mapped to html in Apache. otherwise cache is served as plain text
-  
+
 
 # external
 #  map.connect 'ext/:feedName', :controller=> 'feed', :action => 'external',
