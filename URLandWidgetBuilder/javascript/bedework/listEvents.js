618bf78ae181cca037b018cb5f1987e3c15e145c
Arlen Johnson <johnsa@rpi.edu>
2010-01-06 21:40:18 +0000

    updates to listEvents widget functions

--- URLandWidgetBuilder/javascript/bedework/listEvents.js
+++ URLandWidgetBuilder/javascript/bedework/listEvents.js
@@ -11,14 +11,11 @@ var bwJsWidgetOptions = {
 };
 
 // Insert Bedework calendar events from a json feed
-function insertEvents(outputContainerID) {
+function insertBwEvents(outputContainerID) {
   var outputContainer = document.getElementById(outputContainerID);
   var output = "";
   var eventlist = new Array();
   var eventIndex = 0;
-  
-  // Include the urlPrefix for links back to events in the calendar. 
-  var urlPrefix = bwJsWidgetOptions.calendarServer + bwJsWidgetOptions.calSuiteContext + "/event/eventView.do";
 
   if ((bwObject.bwEventList != undefined) &&
       (bwObject.bwEventList.events != undefined) &&
@@ -36,6 +33,7 @@ function insertEvents(outputContainerID) {
     
     // The title is included because you may wish to hide it when 
     // no events are present.
+    if ()
     output += "<h3 id=\"bwEventsTitle\">" + bwJsWidgetOptions.title + "</h3>";
     output += "<ul id=\"bwEventList\">";
     
@@ -44,31 +42,19 @@ function insertEvents(outputContainerID) {
       // provide a shorthand reference to the event:
       var event = bwObject.bwEventList.events[eventlist[i]];
       
-      // generate the query string parameters that get us back to the 
-      // event in the calendar:
-      var eventQueryString = "?subid=" + event.subscriptionId;
-      eventQueryString += "&amp;calPath=" + event.calPath;
-      eventQueryString += "&amp;guid=" + event.guid;
-      eventQueryString += "&amp;recurrenceId=" + event.recurrenceId;
-      
       // create the list item:
       output += "<li>";
-      output += "<strong class=\"dateTime\">";
-      // don't include times for now
-      if ((event.start.shortdate != event.end.shortdate) && (event.start.datetime.substring(0,4) == event.end.datetime.substring(0,4))) {
-        output += event.start.longdate.substring(0,event.start.longdate.indexOf(","));
-        output += "-" + event.end.datetime.substring(6,8) + ", ";
-        output += event.end.datetime.substring(0,4);
-      } else if ((event.start.shortdate != event.end.shortdate) && (event.start.datetime.substring(0,4) != event.end.datetime.substring(0,4))) {
-        output += event.start.longdate + " - ";
-        output += event.end.longdate + " ";
-      }
-      else {
-        output += event.start.longdate + " ";
-      }
-      output += "</strong><br/>";
       
-      output += "<a href=\"" + urlPrefix + eventQueryString + "\">" + event.summary + "</a>";
+      if (bwJsWidgetOptions.listMode = 'byDate') {
+        output += formatDateTime(event);
+        output += "<br/>"
+        output += formatSummary(event,outputContainerID,i);
+      } else {
+        output += formatSummary(event,outputContainerID,i);
+        output += "<br/>"
+        output += formatDateTime(event);
+      }
+      
       if (bwJsWidgetOptions.displayLocationInList = "true") {
         output += "<br/>" + event.location.address;
       }
@@ -82,93 +68,103 @@ function insertEvents(outputContainerID) {
   }
 }
 
+function outputBwDateTime(event) {
+  var output;
+  output += "<strong class=\"dateTime\">";
+  // don't include times for now
+  if ((event.start.shortdate != event.end.shortdate) && (event.start.datetime.substring(0,4) == event.end.datetime.substring(0,4))) {
+    output += event.start.longdate.substring(0,event.start.longdate.indexOf(","));
+    output += "-" + event.end.datetime.substring(6,8) + ", ";
+    output += event.end.datetime.substring(0,4);
+  } else if ((event.start.shortdate != event.end.shortdate) && (event.start.datetime.substring(0,4) != event.end.datetime.substring(0,4))) {
+    output += event.start.longdate + " - ";
+    output += event.end.longdate + " ";
+  }
+  else {
+    output += event.start.longdate + " ";
+  }
+  output += "</strong>";
+  
+  return output;
+}
 
+function outputBwSummary(event,outputContainerId,i) {
+  var output;
 
-function insertEvents(outputContainerID) {
+  if (bwJsWidgetOptions.displayEventDetailsInline = 'false') { 
+    // link back to the calendar
+    
+    // Include the urlPrefix for links back to events in the calendar. 
+    var urlPrefix = bwJsWidgetOptions.calendarServer + bwJsWidgetOptions.calSuiteContext + "/event/eventView.do";
+    
+    // generate the query string parameters that get us back to the 
+    // event in the calendar:
+    var eventQueryString = "?subid=" + event.subscriptionId;
+    eventQueryString += "&amp;calPath=" + event.calPath;
+    eventQueryString += "&amp;guid=" + event.guid;
+    eventQueryString += "&amp;recurrenceId=" + event.recurrenceId;
+    
+    output += "<a href=\"" + urlPrefix + eventQueryString + "\">" + event.summary + "</a>";
+    
+  } else { 
+    // don't link back to the calendar - display event details in the widget
+    output += "<a href=\"javascript:showEvent('" + outputContainerID + "'," + i + ");\">" + event.summary + "</a><br/>";
+      
+  }
+  
+  return output;
+}
+
+      
+function showBwEvent(outputContainerID, eventId) {
   var outputContainer = document.getElementById(outputContainerID);
   var output = "";
-  var eventlist = new Array();
-  var eventIndex = 0;
-      
-  if ((bwObject.bwEventList != undefined) && (bwObject.bwEventList.events != undefined) && 
-      (bwObject.bwEventList.events.length > 0)) {
-    // get events 
-    for (i = 0; i < bwObject.bwEventList.events.length; i++) {
-      eventlist[eventIndex] = i;
-      eventIndex++;
-    }
-          
-    // The title is included here because you may wish to hide it when 
-    // no events are present.
-    output += "<h1 id=\"bwEventsTitle\">Upcoming Events</h1>";
-    output += "<p>Events for the next seven days:</p>";
-    output += "<ul id=\"bwEventList\">";
-          
-    // Now, iterate over the events:
-    for(i in eventlist){
-      // provide a shorthand reference to the event:
-      var event = bwObject.bwEventList.events[eventlist[i]];
-            
-      // create the list item:
-      output += "<li>";
-      output += "<a href=\"javascript:showEvent('" + outputContainerID + "'," + i + ");\">" + event.summary + "</a><br/>";
-      output += event.location.address + " - ";
-      output += event.calendar.name + "<br/>";
-      output += event.start.longdate + " ";
-      output += event.start.time;
-      if ((event.start.shortdate != event.end.shortdate) && (event.start.time != event.end.time)) {
-        output += " - " + event.end.longdate + " ";
-        output += event.end.time;
-      } else if (event.start.time != event.end.time) {
-        output += " - " + event.end.time;
-      }           
-      output += "</li>";
-    }
-    output += "</ul>";
-          
-   // Finally, send the output to the container:
-   outputContainer.innerHTML = output;
- }
-}
-      
-      function showEvent(outputContainerID, eventId) {
-        var outputContainer = document.getElementById(outputContainerID);
-        var output = "";
-        // provide a shorthand reference to the event:
-        var event = bwObject.bwEventList.events[eventId];
-        
-        // create the event
-        output += "<h1 id=\"bwEventsTitle\">" + event.summary + "</h1>";
-        output += "<table id=\"bwEventLogistics\">";
-        output += "<tr><th>When:</th><td>" + event.start.longdate + " ";
-        output += event.start.time;
-        if ((event.start.shortdate != event.end.shortdate) && 
-            (event.start.time != event.end.time)) {
-          output += " - " + event.end.longdate + " ";
-          output += event.end.time;
-        } else if (event.start.time != event.end.time) {
-          output += " - " + event.end.time;
-        }
-        output += "</td></tr>";
-        if (event.location.link != "") {
-          output += "<tr><th>Where:</th><td><a href=\""+ event.location.link + "\">" + event.location.address + "</a></td></tr>";
-        } else {
-          output += "<tr><th>Where:</th><td>" + event.location.address + "</td></tr>";
-        }
-        
-        output += "<tr><th>Description:</th><td>" + event.description + "</td></tr>";
-        output += "<tr><th>Calendar:</th><td>" + event.calendar.name + "</td></tr>";
-        if (event.categories != "") {
-          output += "<tr><th>Categories:</th><td>" + event.categories + "</td></tr>";
-        }
-        if (event.link != "") {
-          output += "<tr><th>See:</th><td><a href=\"" + event.link + "\">" + event.link + "</a></td></tr>";
-        }
-        output += "</table>";
-        
-        // create a link back to the main view
-        output += "<p id=\"bwEventsListLink\"><a href=\"javascript:insertEvents('" + outputContainerID + "')\">Return to Event List</a>";
+  // provide a shorthand reference to the event:
+  var event = bwObject.bwEventList.events[eventId];
+  
+  // create the event
+  output += "<h3 id=\"bwEventsTitle\">" + event.summary + "</h3>";
+  output += "<div id=\"bwEventLogistics\">";
+  
+  output += "<span class=\"bwEventDateTime\">"
+  output += event.start.longdate + " ";
+  output += event.start.time;
+  if ((event.start.shortdate != event.end.shortdate) && 
+      (event.start.time != event.end.time)) {
+    output += " - " + event.end.longdate + " ";
+    output += event.end.time;
+  } else if (event.start.time != event.end.time) {
+    output += " - " + event.end.time;
+  }
+  output += "<br/></span>";
+  
+  output += "<span class=\"bwEventLoc\">"
+  if (event.location.link != "") {
+    output += "<a href=\""+ event.location.link + "\">" + event.location.address + "</a>";
+  } else {
+    output += event.location.address;
+  }
+  output += "<br/></span>";
+  
+  output += "<span class=\"bwEventDesc\">"
+  output += event.description;
+  output += "<br/></span>";
 
-        // Send the output to the container:
-        outputContainer.innerHTML = output;
-      }
+  if (event.categories != "") {
+    output += "<span class=\"bwEventCats\">"
+    output += event.categories;
+    output += "<br/></span>";
+  }
+  if (event.link != "") {
+    output += "<span class=\"bwEventLink\">"
+    output += "See: <a href=\"" + event.link + "\">" + event.link + "</a>";
+    output += "</span>";
+  }
+  output += "</div>";
+  
+  // create a link back to the main view
+  output += "<p id=\"bwEventsListLink\"><a href=\"javascript:insertEvents('" + outputContainerID + "')\">Return</a>";
+
+  // Send the output to the container:
+  outputContainer.innerHTML = output;
+}
